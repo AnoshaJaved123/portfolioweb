6779e86d3b5470efb7288f37ceb2f2295f5f04c1
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-09-08 17:06:05 +0500

    new change

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,9 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Finaly Runs 
+    <div >
+     
+      Finaly Runs on browser
     </div>
   )
 }
