b0f31dcbe04b908bf2a13be94899d3fbfff751d7
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-09-10 11:46:40 +0500

    today changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-import Header from './Components/header'
-import Footer from './Components/footer'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 
 function MyApp({ Component, pageProps }) {
